8df1e2686a8e4d70e84b04c61304feaee9240a27
Johna N. Morris <johna.n.morris@gmail.com>
2016-11-15 15:02:56 -0800

    implemented score(word) function

--- scrabble.js
+++ scrabble.js
@@ -1,8 +1,46 @@
-var Scrabble = function() {};
+var Scrabble = function() {
+  //   LETTERS = {
+  //     1 => %w(A E I O U L N R S T),
+  //     2 => %w(D G),
+  //     3 => %w(B C M P),
+  //     4 => %w(F H V W Y),
+  //     5 => %w(K),
+  //     8 => %w(J X),
+  //     10 => %w(Q Z)
+  //   }
+  //
 
-// YOUR CODE HERE
-Scrabble.prototype.helloWorld = function() {
-  return 'hello world!';
+
 };
+
+var letters = {
+  A: 1, B: 3, C: 3,
+  D: 2, E: 1, F: 4,
+  G: 2, H: 4, I: 1,
+  J: 8, K: 5, L: 1,
+  M: 3, N: 1, O: 1,
+  P: 3, Q: 10, R: 1,
+  S: 1, T: 1, U: 1,
+  V: 4, W: 4, X: 8,
+  Y: 4, Z: 10
+};
+
+Scrabble.prototype.score = function(word) {
+  var score = 0;
+  var userWord = word.toUpperCase();
+
+  for (var i = 0; i < userWord.length; i++) {
+    for (var letter in letters) {
+      if (letter == userWord[i]) {
+        score += letters[letter];
+      }
+    }
+  }
+  return score;
+};
+
+var myScrabble = new Scrabble();
+
+myScrabble.score("hello");
 
 module.exports = Scrabble;
